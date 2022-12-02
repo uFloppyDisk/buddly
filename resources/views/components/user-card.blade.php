b4457c1b3bf53a7c579d11ef6ac5816a5fc02c90
uFloppyDisk <floppydisk@floppydiskprod.com>
2022-12-02 00:39:20 -0800

    Using component to display user age and gender;

--- resources/views/components/user-card.blade.php
+++ resources/views/components/user-card.blade.php
@@ -22,35 +22,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-auto px-1 align-self-start">
-            @if (!is_null($profile->birthdate))
-                @php
-                    $birthdate = date_create($profile->birthdate);
-                    $today = date_create(date('Y-m-d', time()));
-                @endphp
-                {{ date_diff($birthdate, $today)->format('%y') }}
-            @else
-                {{ __('?')}}
-            @endif
-        </div>
-        <div class="col-auto px-1 align-self-start">
-            <div class="icon d-flex pt-2 align-items-center justify-content-center">
-            @switch($profile->gender)
-                @case(0)
-                    <span class="fa-solid fa-mars"></span>
-                    @break
-                @case(1)
-                    <span class="fa-solid fa-venus"></span>
-                    @break
-                @case(2)
-                    <span class="fa-solid fa-transgender"></span>
-                    @break
-            
-                @default
-                    <span class="fa-solid fa-genderless"></span>
-            @endswitch
-            </div>
-        </div>
+        <x-user-demographic :birthdate="$profile->birthdate" :gender="$profile->gender"/>
     </div>
     <div class="row align-items-end">
         <div class="col ms-2">
